26f0af3e966aef49295635892747edf6cabfe7dc
psbanerjee <78534016+psbanerjee@users.noreply.github.com>
2025-08-04 17:04:16 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,9 @@
+#Input number
+x=input("Enter a number: ")
+#Converting the number to an integer
+x=int(x)
+#The if condition checks if the number is odd or even
+if (x%2)==1:
+    print(x," is an odd number")
+else:
+    print(x," is an even number")
---
+++ Task 2.py
@@ -0,0 +1,6 @@
+start=1 #starting point
+end=50  #end point
+sum = 0 #initialization of sum
+for i in range(start,end+1,1): # For loop for the sum
+    sum = sum + i   #sum in iteration
+print("The sum of numbers from ",start, "to", end, "is: ",sum)  #Final output
